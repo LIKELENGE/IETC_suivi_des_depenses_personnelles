b26be10936508a2966c3b3ab6c81cd5cfc65753f
LIKELENGE MOISE <fatakilikelenge@gmail.com>
2025-05-28 23:09:29 +0200

    Implement Transaction class for managing financial transactions

---
+++ models/transaction.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from uuid import uuid4
+class Transaction(ABC):
+    def __init__(self, montant, date_transaction, utilisateur_id, libelle = None):
+        self.id_transaction = str(uuid4())
+        self.montant = montant
+        self.date_transaction = date_transaction
+        self.utilisateur_id = utilisateur_id
+        self.libelle = libelle
+    
+    
+    def convert_class_vers_dict(self):
+        return {
+            "id_transaction": self.id_transaction,
+            "montant": self.montant,
+            "date_transaction": self.date_transaction,
+            "utilisateur_id": self.utilisateur_id
+        }
