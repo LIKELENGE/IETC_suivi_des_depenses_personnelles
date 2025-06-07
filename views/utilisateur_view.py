c38e5387b8844b8711e4738afc9aad7eb1ff4339
ietc-JPierre <nsngmvpeter4@gmail.com>
2025-06-07 10:36:41 +0200

    add inscription method and profile

--- views/utilisateur_view.py
+++ views/utilisateur_view.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect,request, render_template, url_for
 from models.utilisateur import Utilisateur
-from flask_login import login_user
+from flask_login import login_user, current_user, login_required
 
 utilisateur_bp = Blueprint("utilisateur", __name__)
 
@@ -18,16 +18,30 @@ def accueil():
             return redirect(url_for("utilisateur.profil"))
     return render_template("index.html")
 
-# @utilisateur_bp.route("/inscription", methods=["GET", "POST"])
-# def inscription():
-#     if request.method == "POST":
-#         nom = request.form["nom"]
-#         prenom = request.form["prenom"]
-#         email = request.form["email"]
-#         mot_de_passe = request.form["mot_de_passe"]
-#         utilisateur = Utilisateur.ajouter()
-#         if any(u["email"] == email for u in utilisateur):
-#             return "Email déjà utilisé", 409
-#         nouvel_utilisateur = Utilisateur(nom, prenom, email, mot_de_passe)
-#         nouvel_utilisateur.ajouter()
-#         return redirect(url_for("login"))
+@utilisateur_bp.route("/profil/", methods=["GET"])
+@login_required
+def profil():
+
+    utilisateur = {
+        "id_utilisateur": current_user.id_utilisateur,
+        "nom": current_user.nom,
+        "prenom": current_user.prenom,
+        "email": current_user.email
+    }
+    return render_template("profil.html", utilisateur=utilisateur)
+
+
+
+@utilisateur_bp.route("/inscription/", methods=["GET", "POST"])
+def inscription():
+    if request.method == "POST":
+        nom = request.form["nom"]
+        prenom = request.form["prenom"]
+        email = request.form["email"]
+        mot_de_passe = request.form["mot_de_passe"]
+        utilisateur = Utilisateur.ajouter()
+        if any(u["email"] == email for u in utilisateur):
+            return "Email déjà utilisé", 409
+        nouvel_utilisateur = Utilisateur(nom, prenom, email, mot_de_passe)
+        nouvel_utilisateur.ajouter()
+        return redirect(url_for("login"))
