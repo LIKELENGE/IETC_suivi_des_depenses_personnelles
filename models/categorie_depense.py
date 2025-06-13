5a9582436727e5820640ba9d0190be8bb311041b
ietc-JPierre <nsngmvpeter4@gmail.com>
2025-06-13 22:33:48 +0200

    fix: ajoute la clé 'limite' dans convert_class_vers_dict() docs: ajoute des explications du fichier categorie_depense et des commentaire section dans CategorieDepense

--- models/categorie_depense.py
+++ models/categorie_depense.py
@@ -1,3 +1,11 @@
+"""
+Fichier categorie_depense
+
+Ce fichier contient la classe CategorieDepense qui permet de gérer les catégories de dépenses
+d’un utilisateur dans un système de gestion des depenses personnel.
+
+Les données sont stockées dans un fichier JSON via un gestionnaire générique.
+"""
 from uuid import uuid4
 try:
     from .classe_generique import JSONManager
@@ -9,41 +17,53 @@ gestionnaire = JSONManager(chemin)
 
 
 class CategorieDepense:
+    """ Cette classe represente categorie de dépense liées à l'utilisateur de l'application
+    Avec
+    lES attributs;
+        id_categorie (str) : Identifiant unique de la catégorie (UUID).
+        description (str) : Nom ou description de la catégorie (ex : 'loyer', 'courses').
+        limite (float/int) : Montant limite autorisé pour cette catégorie.
+        id_utilisateur (str) : Identifiant de l’utilisateur auquel appartient la catégorie.
+    Les méthodes:
+        ajouter() : Ajoute la catégorie dans le fichier JSON si elle est unique.
+        modifier() : Modifie les propriétés d’une catégorie existante.
+        supprimer() : Supprime une catégorie existante.
+        lister_categorie_par_personne() : Lister les catégories liées à un utilisateur.
+        afficher_categorie() : Afficher une catégorie spécifique par son ID.
+    """
     def __init__(self, description, id_utilisateur, limite,id_categorie=None):
         self.id_categorie = id_categorie or str(uuid4())
         self.description = description
         self.limite =  limite
         self.id_utilisateur = id_utilisateur
         
-
-
     def convert_class_vers_dict(self):
         return {
             "id_categorie": self.id_categorie,
             "description": self.description,
+            "limite": self.limite,
             "id_utilisateur": self.id_utilisateur,
             
         }
-
-
     @staticmethod
     def verification_unicite(description, id_utilisateur):
         def condition(item):
-            return item["description"] == description and item["id_utilisateur"] == id_utilisateur
+            return (
+                item["description"] == description 
+                and item["id_utilisateur"] == id_utilisateur
+            )
         if gestionnaire.lire_avec_conditions(condition):
             print("Cette catégorie de dépense existe déjà pour cet utilisateur.")
             return False
         return True
-    
+ 
     def ajouter(self):
         if self.verification_unicite(self.description, self.id_utilisateur):
             gestionnaire.ajouter(self.convert_class_vers_dict())
 
-    
+  
     @staticmethod
     def modifier(id_categorie, **updates):
-
-        # Vérifier unicité si description est modifiée
         if "description" in updates and "id_utilisateur" in updates:
             nouvelle_description = updates["description"]
             id_utilisateur = updates["id_utilisateur"]
@@ -52,7 +72,7 @@ class CategorieDepense:
                 return (
                     item["description"] == nouvelle_description and
                     item["id_utilisateur"] == id_utilisateur and
-                    item["id_categorie"] != id_categorie  # Ne pas comparer avec elle-même
+                    item["id_categorie"] != id_categorie 
                 )
 
             deja_existe = gestionnaire.lire_avec_conditions(meme_description)
@@ -71,15 +91,30 @@ class CategorieDepense:
         gestionnaire.modifier(condition, update)
         print(f"Catégorie '{id_categorie}' modifiée.")
 
+    @staticmethod
+    def afficher_categorie(id_categorie):
+        def condition(item):
+            return item["id_categorie"] == id_categorie
 
+        resultats = gestionnaire.lire_avec_conditions(condition)
+        
+        if resultats:
+            return CategorieDepense(**resultats[0]) 
+        return None
 
     @staticmethod
-    def lister_categorie_par_personne(utilisateur_id):
-        """cette méthode permet de lister les catégories de dépenses par utilisateur"""
+    def lister_categorie_par_personne(id_utilisateur):
         def condition(item):
-            return item['utilisateur_id'] == utilisateur_id
-        categories = gestionnaire.lister(['utilisateur_id'] == utilisateur_id)
-        return [categorie(**categorie) for categorie in categories]
+            return item["id_utilisateur"] == id_utilisateur
+        categories_depenses = gestionnaire.lire_avec_conditions(condition)
+        return [CategorieDepense(**categorie_depense) for categorie_depense in categories_depenses]
+
+    @staticmethod
+    def supprimer(id_categorie):
+        def condition(item):
+            return item["id_categorie"] == id_categorie
+
+        gestionnaire.supprimer(condition)
 
 
 #cas d'utilisation ajouter
