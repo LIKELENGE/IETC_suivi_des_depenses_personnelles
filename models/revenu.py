1d88a7e90974a8b45429439632d854b15dd28d21
Abiel-Jason <92023252+Ajsama@users.noreply.github.com>
2025-06-15 21:56:35 +0200

    documentation sur le fichier revenu

--- models/revenu.py
+++ models/revenu.py
@@ -1,3 +1,11 @@
+"""
+Fichier revenu
+
+Ce fichier contient la classe Revenu, qui hérite de la classe Transaction.
+
+Elle permet de gérer les revenus d’un utilisateur dans une application de gestion financière.
+Les revenus sont stockés dans un fichier JSON via un gestionnaire générique.
+"""
 try:
     from .transaction import Transaction
     from .classe_generique import JSONManager
@@ -8,6 +16,24 @@ except ImportError:
 chemin = "data/revenus.json"   
 gestionnaire = JSONManager(chemin)
 class Revenu(Transaction):
+    """
+    Cette classe représente un revenu enregistré par un utilisateur.
+
+    Attributs :
+        id_transaction (str) : Identifiant unique hérité de la classe Transaction.
+        montant (float/int) : Montant du revenu.
+        date_transaction (str) : Date à laquelle le revenu a été enregistré.
+        utilisateur_id (str) : Identifiant de l’utilisateur concerné.
+        libelle (str) : Description facultative du revenu.
+        imposable (bool) : Indique si le revenu est imposable (par défaut : True).
+
+    Méthodes :
+        convert_class_vers_dict() : Convertit l'objet en dictionnaire pour le stockage JSON.
+        ajouter() : Ajoute un revenu au fichier s’il est valide.
+        modifier(id_transaction, **updates) : Modifie un revenu existant selon l’ID.
+        supprimer(id_transaction) : Supprime un revenu par son ID.
+        revenue_par_utilisateur(utilisateur_id) : Liste les revenus liés à un utilisateur donné.
+    """
     def __init__(self, montant, date_transaction, utilisateur_id, imposable=True, libelle=None):
         super().__init__(montant, date_transaction, utilisateur_id, libelle)
         self.imposable = imposable
